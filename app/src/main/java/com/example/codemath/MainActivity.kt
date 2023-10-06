beb0334fd68fc9232ce6ce5f5357c07fdabca623
PurpleFlower436 <bhargvih12@gmail.com>
2023-10-06 08:36:25 -0500

    first number field works. now add the second number field and get the inputs added together

--- app/src/main/java/com/example/codemath/MainActivity.kt
+++ app/src/main/java/com/example/codemath/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.codemath
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val numberInput1EditText = findViewById<EditText>(R.id.number_input1)
+
+
     }
+
+
 }
